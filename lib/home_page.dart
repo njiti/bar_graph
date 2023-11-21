75dcf89966fd039090c03b42a6715f451796ef08
njiti <kamaukan77@gmail.com>
2023-11-21 12:59:35 +0300

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:bar_graph/bar%20graph/bar_graph.dart';
+import 'package:flutter/material.dart';
+
+/*
+  TASK!
+  Input: List of weekly expenses = [sunAmount, monAmount, .. , satAmount ]
+  Output: Display in a nice bar graph :D
+*/
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // list of weekly expense
+  List<double> weeklySummary = [
+    4.40,
+    2.50,
+    42.42,
+    10.50,
+    100.20,
+    88.99,
+    90.10,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: SizedBox(
+          height: 400,
+          child: MyBarGraph(
+              weeklySummary: weeklySummary,
+          ),
+        ),
+      ),
+    );
+  }
+}
